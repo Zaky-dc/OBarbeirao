a6e8b068c957970f07caa5eabd534ccfb47b45c0
zakir magide <zakirmagide@gmail.com>
2025-11-10 17:23:51 +0200

    actualizacao

--- admin/src/components/ListaServicos.jsx
+++ admin/src/components/ListaServicos.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function ListaServicos() {
   const [servicos, setServicos] = useState([]);
-  const BASE_URL = import.meta.env.VITE_API_URL;
+  const BASE_URL ="https://o-barbeirao-back.vercel.app/api";
 
   useEffect(() => {
     axios.get(`${BASE_URL}/servicos`).then((res) => setServicos(res.data));
